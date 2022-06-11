4f5dbd30dd251d6b3eef1fe02d7034c6d8b28fd0
Brian Boy <info@derseitenschneider.ch>
2022-06-11 19:06:10 +0200

    Setup abstract logic with color groups

--- src/index.js
+++ src/index.js
@@ -9,21 +9,21 @@ const teacher = {
 
 // Available hours students
 
-const student1 = {
+const eliel = {
   lessonDuration: 4 / 6,
   option1: {
     start: 12,
     end: 15,
   },
 };
-const student2 = {
+const sandra = {
   lessonDuration: 0.5,
   option1: {
     start: 13,
     end: 14,
   },
 };
-const student3 = {
+const simona = {
   lessonDuration: 0.5,
   option1: {
     start: 12,
@@ -31,12 +31,62 @@ const student3 = {
   },
 };
 
-const slotsArr = [];
+const slotsArr = {};
 let start = teacher.start;
 
-for (let i = 0; i < (teacher.end - teacher.start) * 6; i++) {
-  slotsArr.push(Number(start.toFixed(2)));
-  start += 1 / 6;
+for (let i = 0; i < (teacher.end - teacher.start) * 12; i++) {
+  //Push rounded starting number in array
+  // slotsArr.push(Number(start.toFixed(2)));
+  slotsArr[start.toFixed(2)] = undefined;
+  start += 1 / 12;
 }
 
-console.table(slotsArr);
+// console.table(Object.keys(slotsArr));
+
+//Abstract
+
+const red = 3;
+const blue = 2;
+const green = 4;
+
+const clrObj = {};
+let startObj = 0;
+
+//Creating object whit numbered keys
+for (let i = 0; i < 20; i++) {
+  clrObj[startObj] = undefined;
+  startObj++;
+}
+
+//Looping through colors
+let countRed = red;
+for (let i = 0; i < Object.keys(clrObj).length; i++) {
+  if (clrObj[i] === undefined) {
+    if (countRed > 0) {
+      clrObj[i] = "red";
+      countRed--;
+    }
+  }
+}
+
+let countBlue = blue;
+for (let i = 0; i < Object.keys(clrObj).length; i++) {
+  if (clrObj[i] === undefined) {
+    if (countBlue > 0) {
+      clrObj[i] = "blue";
+      countBlue--;
+    }
+  }
+}
+
+let countGreen = green;
+for (let i = 0; i < Object.keys(clrObj).length; i++) {
+  if (clrObj[i] === undefined) {
+    if (countGreen > 0) {
+      clrObj[i] = "green";
+      countGreen--;
+    }
+  }
+}
+
+console.table(clrObj);
